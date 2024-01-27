9116dfa6e96c93671562336b138c7318bac43ea9
ifindyoux <hello@ifindyou.co>
2024-01-27 10:17:02 +0000

    update

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>iFindYou.com is Largest Adult Search Platorm </title>
+    <title>iFindYou is Largest Adult Friend Finder Platorm </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {!! $sitesetting['meta_tags'] !!}
     <!-- site favicon -->
